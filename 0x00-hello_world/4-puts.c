52dd5b55301da5882caac2e08eb95ef3bc4f38ac
sportivov6 <edmundmmatta@mail.com>
2022-03-10 14:20:49 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * C program prints Programming is like building multilingual puzzle
+ */
+
+# include <stdio.h>
+
+/**
+ * main -Etry point
+ *
+ * Return: ALways 0.
+ */
+ 
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
