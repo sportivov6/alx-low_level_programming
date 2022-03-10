d9d0ae66ec67273208d44d6e7e2d97f89def7a33
sportivov6 <edmundmmatta@mail.com>
2022-03-10 14:29:55 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * C program prints with proper grammar, but the outcome is a piece of art
+ */
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Retutn: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return 0;
+}
