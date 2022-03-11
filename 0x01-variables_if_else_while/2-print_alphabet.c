55b16f66199e85c4516ac909ed60751eb95a2f3e
sportivov6 <edmundmmatta@mail.com>
2022-03-11 15:13:06 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/*
+ * 2-print_aplhabet.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main program prints the alphabet in lowercase
+ */
+
+int main (void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter)
+
+	putchar('\n');
+
+	return (0);
+}
