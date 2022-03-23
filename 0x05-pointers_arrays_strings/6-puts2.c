dd7a99ea5ff4a69f12ed7b89525fc39120373c0c
sportivov6 <edmundmmatta@mail.com>
2022-03-23 15:58:34 -0700

    puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+/*
+ * File 6-puts2.c
+ */
+
+#include "main.h"
+
+/**
+ * Print one char out of two of a string
+ *
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n')
+}
