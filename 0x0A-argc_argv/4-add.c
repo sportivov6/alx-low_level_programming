be2c600b300921c1b11b9ad55ddde80be8f6cd9d
sportivov6 <edmundmmatta@mail.com>
2022-04-06 02:23:48 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,29 +6,27 @@
 #include<sstdlib.h>
 
 /**
- * main - print the multiplication of two numbers.
+ * main - print the addition of numbers.
  * @argc: array length.
  * @argv: array.
  *
  * Retun: 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int sum = 0, i, j;
+	int num, digit, sum = 0;
 
-	if (argc > 1)
+	for (num = 1; num < argc; num++)
 	{
-		for (i = 1; i < argc; i++)
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			for (j = 0; argv[i][j] ; j++)
+			if (argv[num][digit] < '0' || argv[num][digit] > '9'))
 			{
-				if (argv[i][j] < '0' || argv[i][j] > '9')
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			sum += atoi(argv[i]);
+
+			sum += atoi(argv[num]);
 		}
 	}
 
