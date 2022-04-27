3a2a83de70d47b0d87281dc06274e1b6f357904f
sportivov6 <edmundmmatta@mail.com>
2022-04-27 12:52:09 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,48 +1,30 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatinate first string
- * and n chars from the second string.
- *
- * @s1: First string .
- * @s2: Second string.
- * @n: Numbers of chars to concatinate from the second string.
- * Return: Pointer to the new string.
+ * _calloc - Allocates memory for an array of a certain number of elements each of an inputted byte size.
+ * @nmemb: The number of elements.
+ * @size: The byte size of each array element.
+ * Return: If nmemb = 0, size = 0, or the function fails - NULL. Otherwise - a pointer to the allocated memory.
  **/
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *s;
-	unsigned int s1_length = 0, s2_length = 0, k = 0;
+	void *mem;
+	char *filler;
+	unsigned int index;
 
-	if (s1 != NULL)
-		for (; s1[s1_length]; s1_length++)
-			;
-	if (s2 != NULL)
-		for (; s2[s2_length]; s2_length++)
-			;
-	if (n >= s2_length)
-		s = malloc(sizeof(char) * (s1_length + s2_length + 1));
-	else
-	{
-		s = malloc(sizeof(char) * (s1_length + n + 1));
-		s2_length = n;
-	}
-	if (s == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	while (k < s1_length)
-	{
-		s[k] = s1[k];
-		k++;
-	}
+	mem = malloc(size * nmemb);
 
-	while (k < s1_length + s2_length)
-	{
-		s[k] = s2[k - s1_length];
-		k++;
-	}
-	s[k] = '\0';
-	return (s);
+	if (mem == NULL)
+		return (NULL);
+
+	filler = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+		filler[index] = '\0';
+
+	return (mem);
 }
