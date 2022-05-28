7c0c1d164e3a943adfe0805b5fdb191a485f77f2
sportivov6 <edmundmmatta@mail.com>
2022-05-28 05:29:37 -0700

    header file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,17 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Descriptin: Header file
- */
-
-#include <stddef.h>
-
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char *accept);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
